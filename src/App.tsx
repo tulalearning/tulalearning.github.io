2eb0cc7c103f1dffa95d31c35392b370d9cacbc1
tulalearning <tulalearning@gmail.com>
2023-06-03 17:57:45 +0700

    POR-21 - Setup Project Using React

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <img
+        src="../public/assets/under_construction.jpg"
+        alt="Under Construction Page"
+        style={{ width: '50%', objectFit: 'cover' }}
+      />
+    </div>
+  )
+}
+
+export default App
